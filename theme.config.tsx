3d3019c33063bee123be048fd2e1e5b7fad34e6c
iamspdarsan <iamspdarsan@gmail.com>
2024-05-04 03:18:42 +0530

    initial setup

---
+++ theme.config.tsx
@@ -0,0 +1,22 @@
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  logo: (
+    <p>
+      <b>RICHIE</b> JS
+    </p>
+  ),
+  logoLink: "/",
+  project: {
+    link: "https://github.com/cresteem/Richie-JS",
+  },
+  chat: {
+    link: "https://t.me/richiejs_cresteem",
+  },
+  docsRepositoryBase: "https://github.com/cresteem/Richie-JS-DocSite",
+  footer: {
+    text: "Richie JS API References",
+  },
+};
+
+export default config;
