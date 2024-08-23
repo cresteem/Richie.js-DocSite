76cdb6babaa3cb34012667224eeb35014c8302ea
DARSAN <iamspdarsan@gmail.com>
2024-08-23 19:12:38 +0530

    favicons added

---
+++ gen-favicons.mjs
@@ -0,0 +1,79 @@
+import { favicons } from "favicons";
+import { mkdirSync, writeFileSync } from "fs";
+import { basename, join } from "path";
+
+export async function generateFavicons(
+  metaDesc,
+  version,
+  baseImage = "fav.png",
+  appName = basename(process.cwd()),
+  basepath = join(process.cwd(), "public"),
+  faviconsPath = "favicons",
+  devName = "Darsan",
+  devWebsite = "https://darsan.in"
+) {
+  const options = {
+    path: faviconsPath,
+
+    appName: appName,
+    appDescription: metaDesc,
+    version: version,
+
+    developerName: devName,
+    developerURL: devWebsite,
+
+    background: "#fff",
+    theme_color: "#fff",
+    appleStatusBarStyle: "black-translucent",
+
+    display: "fullscreen",
+    orientation: "natural",
+
+    start_url: "/",
+
+    manifestMaskable: true,
+
+    icons: {
+      android: true,
+      appleIcon: true,
+      favicons: true,
+      yandex: true,
+      appleStartup: false,
+      windows: false,
+    },
+
+    shortcuts: {},
+  };
+
+  try {
+    const { images, files, html } = await favicons(baseImage, options);
+
+    const rootPath = join(basepath, faviconsPath);
+    mkdirSync(rootPath, { recursive: true });
+
+    /* write images */
+    images.forEach((image) => {
+      writeFileSync(join(rootPath, image.name), image.contents, {
+        encoding: "binary",
+      });
+    });
+
+    /* write config files */
+    files.forEach((file) => {
+      writeFileSync(join(rootPath, file.name), file.contents, {
+        encoding: "utf8",
+      });
+    });
+
+    const faviconsHtmlLinks = html.join("");
+    writeFileSync(join(basepath, "links.html"), faviconsHtmlLinks);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+}
+
+generateFavicons(
+  "Meet Richie JS, a Rich Results Generator, Boost your website's visibility with Richie JS, a powerful SEO tool that generates rich results for enhanced search engine rankings and improved user experience. Learn how to simplify your SEO process and increase your online presence with our innovative solution.",
+  "1.1.5"
+);
--- theme.config.tsx
+++ theme.config.tsx
@@ -6,13 +6,105 @@ import { useRouter } from "next/router";
 import { RiTelegramFill } from "react-icons/ri";
 
 const config: DocsThemeConfig = {
+  head: (
+    <>
+      <link rel="icon" type="image/x-icon" href="/favicons/favicon.ico" />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicons/favicon-16x16.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicons/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="48x48"
+        href="/favicons/favicon-48x48.png"
+      />
+      <link rel="manifest" href="/favicons/manifest.webmanifest" />
+      <meta name="mobile-web-app-capable" content="yes" />
+      <meta name="theme-color" content="#fff" />
+      <meta name="application-name" content="Richie-JS-DocSite" />
+      <link
+        rel="apple-touch-icon"
+        sizes="57x57"
+        href="/favicons/apple-touch-icon-57x57.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="60x60"
+        href="/favicons/apple-touch-icon-60x60.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="72x72"
+        href="/favicons/apple-touch-icon-72x72.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="76x76"
+        href="/favicons/apple-touch-icon-76x76.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="114x114"
+        href="/favicons/apple-touch-icon-114x114.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="120x120"
+        href="/favicons/apple-touch-icon-120x120.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="144x144"
+        href="/favicons/apple-touch-icon-144x144.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="152x152"
+        href="/favicons/apple-touch-icon-152x152.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="167x167"
+        href="/favicons/apple-touch-icon-167x167.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/favicons/apple-touch-icon-180x180.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="1024x1024"
+        href="/favicons/apple-touch-icon-1024x1024.png"
+      />
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta
+        name="apple-mobile-web-app-status-bar-style"
+        content="black-translucent"
+      />
+      <meta name="apple-mobile-web-app-title" content="Richie-JS-DocSite" />
+      <link
+        rel="yandex-tableau-widget"
+        href="/favicons/yandex-browser-manifest.json"
+      />
+    </>
+  ),
   footer: {
     text: <LicenseFooter />,
   },
 
   banner: {
     key: "114",
-    text: "Richie JS version 1.1.4 🚀",
+    text: "Richie JS version 1.1.5 🚀",
     dismissible: true,
   },
 
@@ -46,7 +138,7 @@ const config: DocsThemeConfig = {
     const url = `https://cresteem.com${basePath}${pathname}`;
     const title =
       frontMatter.title +
-      " | Richie JS API References | OpenSource at Cresteem";
+      " | Richie JS API References | OpenSource of Cresteem";
     return {
       title: title,
       nofollow: false,
